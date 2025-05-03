c0420849758c0a0426b6ceba20f0bd7c18cc31bb
mvolk420 <mvolk587@gmail.com>
2025-05-03 21:42:23 +0200

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+import string 
+alphabet = list(string.ascii_lowercase)
+direction = input("type encode to encrypt or decode to decrypt ")
+text = input("type your message ")
+shift = int(input("type the shift number "))
+
+def encrypt(original_text : str, shift : int) -> str:
+    encrypted_text = ""
+    for char in original_text:
+        index = alphabet.index(char)
+        shifted_index = (index + shift) % 26
+        encrypted_text += alphabet[shifted_index]
+    
+    return encrypted_text
+
+def decrypt(encrypted_text : str, shift : int) -> str:
+    original_text = ""
+    for char in encrypted_text:
+        index = alphabet.index(char)
+        shifted_index = (index - shift) % 26
+        original_text += alphabet[shifted_index]
+    
+    return original_text
+
+if direction == "encode":
+    encrypted_text = encrypt(text, shift)
+    print("encrypted text:" + encrypted_text)
+else:
+    original_text = decrypt(text, shift)
+    print("original text: " +original_text)
